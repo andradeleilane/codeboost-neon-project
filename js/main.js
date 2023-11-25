08a60d990de498e3a9f750a71c7abb29ef330e3d
Leilane Andrade <andradeleilane25@gmail.com>
2023-11-25 18:35:54 -0300

    finishing desktop version

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".js-slide-review", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    pagination: {
+      el: ".s-review .top .swiper-pagination",
+      clickable: true,
+    },
+});
