ca9b04c7e605602e3506ec5956ff3d19e382f548
Leilane Andrade <andradeleilane25@gmail.com>
2023-11-27 16:06:42 -0300

    add menu mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+const btnDropdown = document.querySelector('.js-btn-dropdown');
+const dropdown = document.querySelector('.js-dropdown');
+
+function openDropdown(event) {
+  event.preventDefault();
+  //console.log('ativar')
+  dropdown.classList.toggle('active');
+}
+
+btnDropdown.addEventListener('click', openDropdown);
+dropdown.addEventListener('mouseleave', openDropdown);
+
+const header = document.getElementById('js-header');
+
+function fixedMenu() {
+  //console.log('scroll');
+  //console.log(window.scrollY);
+  if(window.scrollY > 80) {
+    header.classList.add('fixed-menu');
+  } else {
+    header.classList.remove('fixed-menu');
+  }
+}
+
+document.addEventListener('scroll', fixedMenu);
+
+const btnMenu = document.getElementById('js-btn-menu-mobile');
+const overlayMenu = document.querySelector('.js-overlay');
+
+function openMenuMobile() {
+  //console.log('ativar')
+  document.documentElement.classList.add('menu-opened');
+}
+
+function closeMenuMobile() {
+  document.documentElement.classList.remove('menu-opened');
+  //console.log('fechar')
+}
+
+btnMenu.addEventListener('click', openMenuMobile);
+overlayMenu.addEventListener('click', closeMenuMobile);
+
 var swiper = new Swiper(".js-slide-review", {
     slidesPerView: 3,
     spaceBetween: 32,
@@ -20,6 +62,7 @@ var swiper = new Swiper(".js-slide-review", {
       }
     }
 });
+
 
 AOS.init({
   duration: 1000,
