4d6afbf644a39e61d838393b0c1b7c621eb548da
Leilane Andrade <andradeleilane25@gmail.com>
2023-11-27 00:26:41 -0300

    finishing responsive

--- js/main.js
+++ js/main.js
@@ -5,4 +5,18 @@ var swiper = new Swiper(".js-slide-review", {
       el: ".s-review .top .swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.3,
+        spaceBetween: 16,
+      },
+      600: {
+        slidesPerView: 2.3,
+        spaceBetween: 15,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      }
+    }
 });
